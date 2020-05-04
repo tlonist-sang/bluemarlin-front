1919fcefac43032135896a37e590ac42589cef5d
Harry Kim <tlonist.harry@gmail.com>
2020-05-04 18:51:49 +0900

    loginform-remade

--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useForm} from "react-hook-form";
 import {requestLogin} from "../api/loginAPI";
 import {useDispatch} from "react-redux";
 import {logIn, logOut} from "../actions";
@@ -6,9 +7,12 @@ import bluemarlin from "../icons/bluemarlin.png"
 
 const LoginForm = () => {
     const dispatch = useDispatch();
+    const { register, handleSubmit, watch, errors } = useForm();
+    const [username, setUsername] = useState(null);
+    const [password, setPassword] = useState(null);
 
-    const handleSubmit = (username, password) => {
-        const {status, token} = requestLogin(username, password);
+    const onSubmit = async () => {
+        const {status, token} = await requestLogin(username, password);
         if(status === 'success'){
             dispatch(logIn(username));
         }else{
@@ -17,26 +21,24 @@ const LoginForm = () => {
     }
 
     return (
-        <div className={"ui middle aligned center aligned grid"}>
+        <div className={"ui center aligned grid"} style={{"margin-top":"200px"}}>
             <div className={"column"}>
-                <h2 className={"ui image header"}>
-                    <img className={"logo"} src="/icons/bluemarlin.png"/>
-                </h2>
-                <form className={"ui large form"}>
+                <img className={"logo"} src={bluemarlin}/>
+                <form className={"ui large form form-margin"} onSubmit={handleSubmit(onSubmit)}>
                     <div className={"field"}>
                         <div className={"ui left icon input"}>
-                            <input type={"text"} name={"username"} placeholder={"Enter ID"}/>
+                            <input type={"text"} name={"username"} placeholder={"Enter ID"} onChange={(e)=>setUsername(e.target.value)}/>
                         </div>
                     </div>
                     <div className={"field"}>
                         <div className={"ui left icon input"}>
-                            <input type={"password"} name={"password"} placeholder={"Enter password"}/>
+                            <input type={"password"} name={"password"} placeholder={"Enter password"} onChange={(e)=>{setPassword(e.target.value)}}/>
                         </div>
                     </div>
 
                     <button className={"ui large red forgot button"}>Forgot ID/Password?</button><br/>
                     <div className="ui horizontal divider"></div>
-                    <button className={"ui large green button"} onClick={handleSubmit}>Login</button>
+                    <input type={"submit"} value={"Login"} className={"ui large green login button"}/>
                 </form>
                 <div className={"ui message"}>
                     <button className={"ui large blue register button"}>New to us? Register!</button>
